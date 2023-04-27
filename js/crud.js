0d7975bbb6ffb29b22ed996f88f0495cf7006543
TIAGO RABELLO BRAZ <tiago.rbraz@senacsp.edu.br>
2023-04-26 21:08:32 -0300

    crud

---
+++ js/crud.js
@@ -0,0 +1,45 @@
+document.querySelector("#salvar").addEventListener("click", cadastrar)
+
+function cadastrar() {
+    const titulo = document.querySelector("#titulo").value
+    const pontos = document.querySelector("#pontos").value
+    const categoria = document.querySelector("#categoria").value
+
+    const tarefa = {
+        titulo,
+        pontos,
+        categoria
+    }
+
+    document.querySelector("#tarefas").innerHTML += (criarCard(tarefa))
+}
+
+function apagar(botao) {
+    botao.parentNode.parentNode.parentNode.remove()
+}
+
+function criarCard(tarefa) {
+    const card = `
+        <div class="card col-lg-3 col-md-6 col-sm-12">
+                <div class="card-header">
+                    ${tarefa.titulo}
+                </div>
+                <div class="card-body">
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <p class="card-text">${tarefa.categoria}</p>
+                    <span class="badge text-bg-warning">${tarefa.pontos}pt</span>
+                </div>
+
+                <div class="card-footer">
+                    <a href="#" class="btn btn-success" title="marcar como concluída">
+                        <i class="bi bi-check-circle"></i>
+                    </a>
+                    <a href="#" onClick="apagar(this)" class="btn btn-danger" title="apagar tarefa">
+                        <i class="bi bi-x-circle"></i>
+                    </a>
+                </div>
+
+        </div>
+    `//template literals
+    return card
+}
